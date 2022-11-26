9cd7e5124fd13e1f97b31bc726c00eb6ae4d7e04
MikoreVikt <104897659+MikoreVikt@users.noreply.github.com>
2022-11-26 23:18:23 +0200

    Delete _loader.js

--- src/js/_loader.js
+++
@@ -1,9 +0,0 @@
-/* Loader */
-window.addEventListener('load', () => {
-  const loader = document.querySelector('.loader');
-  loader.classList.add('loader-hidden');
-  loader.addEventListener('transitionend', () => {
-    console.log('loader');
-  });
-});
-// document.body.removeChild('loader');
