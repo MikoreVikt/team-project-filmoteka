325f8725d650d2d77db7695ad51acfeac87cf46c
MikoreVikt <104897659+MikoreVikt@users.noreply.github.com>
2022-11-27 18:42:22 +0200

    adds local-storage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './js/loader/loader';
+import './js/loacal-storage/local-storage';
 import './js/main.js/main-render';
 import './js/film.js/film-modal-window';
 import './js/pagination/pagination';
