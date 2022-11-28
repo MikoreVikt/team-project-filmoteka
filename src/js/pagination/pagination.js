27594206ff666515114183a6aa05936a713be020
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2022-11-28 15:16:53 +0200

    add pagination

--- src/js/pagination/pagination.js
+++ src/js/pagination/pagination.js
@@ -1,2 +1,87 @@
+import { createGallery } from '../main.js/main-render';
 
 let getEl = selector => document.querySelector(selector);
+
+console.log(createGallery());
+
+let currentPage = 1;
+let maxPage = 100;
+let pageCount = 2;
+
+getEl('.gallery');
+getEl('[data-index="1"]').addEventListener('click', onBtnClick);
+getEl('[data-index="2"]').addEventListener('click', onBtnClick);
+getEl('[data-index="3"]').addEventListener('click', onBtnClick);
+getEl('[data-index="4"]').addEventListener('click', onBtnClick);
+getEl('[data-index="5"]').addEventListener('click', onBtnClick);
+
+getEl('.first-button').addEventListener('click', function () {
+  currentPage = 1;
+  render(currentPage);
+});
+
+getEl('.last-button').addEventListener('click', function () {
+  currentPage = maxPage;
+  render(currentPage);
+});
+
+getEl('.arrow-right').addEventListener('click', function () {
+  currentPage += 1;
+  render(currentPage);
+});
+
+getEl('.arrow-left').addEventListener('click', function () {
+  currentPage -= 1;
+  render(currentPage);
+});
+
+function onBtnClick(event) {
+  currentPage = Number(event.target.textContent);
+  render(currentPage);
+}
+
+function firstPage() {
+  currentPage = 1;
+  render(currentPage);
+}
+
+let btns = document.querySelectorAll('.pagination__btn');
+
+function render(pageNumber) {
+  let startPage = Math.max(1, pageNumber - pageCount);
+  let pagesList = [];
+
+  if (startPage + pageCount * 2 > maxPage) {
+    startPage = maxPage - pageCount * 2;
+  }
+
+  for (let i = startPage; i < startPage + 5; ++i) {
+    pagesList.push(i);
+  }
+
+  getEl('[data-index="1"]').textContent = pagesList[0];
+  getEl('[data-index="2"]').textContent = pagesList[1];
+  getEl('[data-index="3"]').textContent = pagesList[2];
+  getEl('[data-index="4"]').textContent = pagesList[3];
+  getEl('[data-index="5"]').textContent = pagesList[4];
+
+  getEl('.arrow-left').hidden = pageNumber <= 1;
+  getEl('.arrow-right').hidden = pageNumber >= maxPage;
+
+  getEl('.dots-left').hidden = pageNumber <= pageCount + 1;
+  getEl('.first-button').hidden = getEl('.dots-left').hidden;
+
+  getEl('.dots-after').hidden = pageNumber >= maxPage - pageCount;
+  getEl('.last-button').hidden = getEl('.dots-after').hidden;
+
+  btns.forEach(el => el.classList.remove('pagination__btn-current'));
+  btns.forEach(el => {
+    if (el.textContent == pageNumber) {
+      el.classList.add('pagination__btn-current');
+    }
+  });
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+window.render = render;
+
