92238a1df39d3d3831868d43f91dea5f9f073fa3
MikoreVikt <104897659+MikoreVikt@users.noreply.github.com>
2022-11-29 21:59:19 +0200

    Update libraryApi.js

--- src/js/fetchApi.js/libraryApi.js
+++ src/js/fetchApi.js/libraryApi.js
@@ -1,18 +1,16 @@
+// import createGallery from '/src/js/main.js';
 
-import createGallery from '/src/js/main.js';
+// const form = document.querySelector(".form");
+// const search = document.querySelector(".input-home");
+// const API_URL_SEARCH = `${BASE_URL}?api_key=${API_KEY}&query=${data}&page=${page}`;
 
-const form = document.querySelector(".form");
-const search = document.querySelector(".input-home");
-const API_URL_SEARCH = `${BASE_URL}?api_key=${API_KEY}&query=${data}&page=${page}`;
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
 
+//     const apiSearchUrl = `${API_URL_SEARCH}${data}`;
+//     if (data) {
+//         createGallery(apiSearchUrl);
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-  
-    const apiSearchUrl = `${API_URL_SEARCH}${data}`;
-    if (data) {
-        createGallery(apiSearchUrl);
-  
-      search.value = "";
-    }
-  });
+//       search.value = "";
+//     }
+//   });
