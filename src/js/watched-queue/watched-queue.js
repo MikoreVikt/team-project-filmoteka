e047fdacaab14f2edcac8ec8446d4285afec4b26
FelixPolkanov <felixpolkanov@gmail.com>
2022-11-30 20:24:09 +0200

    add library-localstrg-render

---
+++ src/js/watched-queue/watched-queue.js
@@ -0,0 +1,106 @@
+import Notiflix from 'notiflix';
+// import { markupCard } from '../watched-queue/watched-queue-markup';
+
+const LOCAL_STORGE_WATCHED = 'local-storage-watched';
+const LOCAL_STORGE_QUEUE = 'local-storage-queue';
+let LocalStgWatchedData = [];
+let LocalStgQueueData = [];
+
+
+const refs = {
+    libraryCurrent: document.querySelector('#library-link'),
+    galleryRef: document.querySelector('.gallery'),
+    watchedButton: document.querySelector('#watched'),
+    queueButton: document.querySelector('#queue'),
+ }
+
+currentPageCheck()
+
+
+function currentPageCheck() {
+    if (!refs.libraryCurrent.classList.contains("current")) {
+     return;
+}
+
+ else if (localStorage.getItem(LOCAL_STORGE_WATCHED) == null ) {
+        Notiflix.Notify.failure('There are no films in WATCHED !');
+addImgIfLocalStgEmpty()
+
+          refs.queueButton.addEventListener('click', onQueueButton);
+        } else {
+       LocalStgWatchedData = JSON.parse(localStorage.getItem(LOCAL_STORGE_WATCHED));
+        let LocalStgData = LocalStgWatchedData;
+        markupCard(LocalStgData)
+    refs.watchedButton.addEventListener('click', onWatchedButton);
+    refs.queueButton.addEventListener('click', onQueueButton);   
+ }
+}
+
+
+function onWatchedButton() {
+    refs.galleryRef.innerHTML = ""; 
+    refs.watchedButton.classList.add("btn-active");
+    refs.queueButton.classList.remove("btn-active");
+    if (localStorage.getItem(LOCAL_STORGE_WATCHED) == null) {
+        Notiflix.Notify.failure('There are no films in WATCHED !');
+        addImgIfLocalStgEmpty()
+    return
+} 
+     LocalStgWatchedData = JSON.parse(localStorage.getItem(LOCAL_STORGE_WATCHED));
+    let LocalStgData = LocalStgWatchedData;
+    markupCard(LocalStgData)     
+}
+
+function onQueueButton() {
+    refs.watchedButton.classList.remove("btn-active");
+    refs.queueButton.classList.add("btn-active");
+    if (localStorage.getItem(LOCAL_STORGE_QUEUE) == null) {
+          refs.galleryRef.innerHTML = ""; 
+        Notiflix.Notify.failure('There are no films in Queue !');
+        addImgIfLocalStgEmpty()
+         refs.watchedButton.addEventListener('click', onWatchedButton)
+    return 
+}
+    
+    LocalStgQueueData = JSON.parse(localStorage.getItem(LOCAL_STORGE_QUEUE));
+    let LocalStgData = LocalStgQueueData;
+    markupCard(LocalStgData)
+    if (localStorage.getItem(LOCAL_STORGE_WATCHED) == null) {
+             addImgIfLocalStgEmpty()
+            refs.watchedButton.addEventListener('click', onWatchedButton)
+}
+}
+
+
+
+ function markupCard(LocalStgData) {
+            const markup = LocalStgData
+            .map(({ alt, date, genre, id, src, vote }) => {
+              
+                const production_date = date.slice(0, 4);
+                const rating = Number(vote).toFixed(1);
+            
+                return `
+                <li class="gallery__item card-set">
+                     <a class="link" href="">
+                       <img class="gallery__img" data-id="${id}" src="https://image.tmdb.org/t/p/w500/${src}" alt="${alt}" loading="lazy" />
+                      <div>
+                          <h2 class="gallery__title">${alt}</h2>
+                         <div class="gallery__wrap">
+                            <p class="gallery__ganres">${genre} | ${production_date}</p>
+                                <p  class="gallery__rating">${rating}</p>
+                            </div>
+                     </div>
+                     </a> 
+                </li>`;  
+            })
+                .join('');
+    refs.galleryRef.innerHTML = markup;  
+}
+
+
+function addImgIfLocalStgEmpty() {
+    refs.galleryRef.innerHTML = '<li ><img class="img-for-empty" src="" alt=" "/> <h2>где-то здесь должна быть картинка при пустом локале</h2></li>'  
+}
+
+
